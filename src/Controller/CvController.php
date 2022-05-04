2bb5f5b328620058af37ce6f9b87fc69f08b35a2
sihamouda <anis.c.hamouda@gmail.com>
2022-05-04 13:01:31 +0100

    part2

--- src/Controller/CvController.php
+++ src/Controller/CvController.php
@@ -8,7 +8,15 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CvController extends AbstractController
 {
-    #[Route("/cv/{lastName}/{firstName}/{age}/{section}", name: 'app_cv')]
+    #[Route(
+        '/cv/{lastName}/{firstName}/{age}/{section}/{_locale?en}',
+        name: 'app_cv',
+        requirements: [
+            '_locale' => 'en|fr',
+            'age' => '\b([0-9]|[1-9][0-9])\b',
+            'section' => 'rt|gl'
+        ]
+    )]
     public function index($lastName, $firstName, $age, $section): Response
     {
         return $this->render('cv/index.html.twig', [
